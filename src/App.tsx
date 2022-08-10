afe91fa50ec7f78a650b900362ab5b3b8501466d
Daniel Serrano Cobos <d.serranoc@outlook.com>
2022-08-10 15:50:57 +0200

    Install ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div className='flex justify-center items-center'>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://tailwindcss.com/" target="_blank">
+        <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
           <img src="/tailwindcss.svg" className="logo" alt="TailwindCSS logo" />
         </a>
+        <a href="https://eslint.org/" target="_blank" rel="noreferrer">
+          <img src="/eslint.svg" className="logo" alt="ESLint logo" />
+        </a>
       </div>
-      <h1>Vite + React + TailwindCSS</h1>
+      <h1>Vite + React + TailwindCSS + ESLint</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
